e7ca4d1b39b63afbfd42cef09b520ff55c5e4c2a
Christoph Werner <christoph.werner@cosuno.de>
2024-01-28 16:39:20 +0100

    Revert "replace url and updated script"
    This reverts commit 8e17635e3a5c0093aff68f8f8c5cbdda2f777a49.

--- src/build-readme.js
+++ src/build-readme.js
@@ -1,23 +1,23 @@
-import RssParser from 'rss-parser'
-import remark from 'remark'
-import zone from 'mdast-zone'
-import { promises } from 'fs'
-import { join, dirname } from 'path'
-import { fileURLToPath } from 'url'
+const RssParser = require('rss-parser')
+const vfile = require('to-vfile')
+const remark = require('remark')
+const zone = require('mdast-zone')
+const { promisify } = require('util')
+const { writeFile } = require('fs')
+const { join } = require('path')
 
 const rssParser = new RssParser()
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 const readmePath = join(__dirname, '..', 'README.md')
 
-const feed = await rssParser.parseURL('https://codepunkt.de/writing/rss.xml')
-const fileContent = await promises.readFile(readmePath)
-const file = await remark()
-  .use(refreshBlogPosts(feed.items.slice(0, 3)))
-  .process(fileContent)
-await promises.writeFile(readmePath, String(file))
+;(async () => {
+  const feed = await rssParser.parseURL('https://codepunkt.de/writing/rss.xml')
+  const file = await remark()
+    .use(refreshBlogPosts(feed.items.slice(0, 3)))
+    .process(vfile.readSync(readmePath))
+  await promisify(writeFile)(readmePath, String(file))
+})()
 
-function refreshBlogPosts (feedItems) {
+function refreshBlogPosts(feedItems) {
   return () => (tree) => {
     zone(tree, 'blog', (start, nodes, end) => {
       return [
@@ -35,10 +35,7 @@ function refreshBlogPosts (feedItems) {
                     children: [
                       {
                         type: 'link',
-                        url: link.replace(
-                          'codepunkt.dewriting',
-                          'codepunkt.de/writing'
-                        ),
+                        url: link,
                         children: [{ type: 'text', value: title }],
                       },
                       { type: 'html', value: '<br/>' },
